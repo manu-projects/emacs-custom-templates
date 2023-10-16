4a5ccabc89e3991e1b40dca3f8a4866f10871f54
Emmanuel <neverkas@proton.me>
2023-10-16 13:03:17 -0300

    Modificado: estructura de directorios

--- init.mk
+++ init.mk
@@ -1,18 +1,21 @@
 # TODO: problemas entre la lectura en la subshell de bash de create-new-project
 CONFIG_FILES=init.mk .dir-locals.el
 
-DIRECTORIES=pages articles
-PAGES=articles courses books issues users-dotfiles documentation tasks videos
-ORG_PAGES=$(addprefix pages/,\
-					$(addsuffix .org,$(PAGES)))
+DIRECTORIES:=pages articles
+DIRECTORIES:=$(addprefix doc/,$(DIRECTORIES))
 
+PAGES:=articles courses books issues users-dotfiles documentation tasks videos
+PAGES:=$(addsuffix .org,$(PAGES))
+PAGES:=$(addprefix doc/pages/,$(PAGES))
+
+# TODO: popup de confirmación, de creación de los directorios
 $(DIRECTORIES):
-	mkdir --verbose $@ \
+	mkdir --verbose --parents $@ \
 	&& touch $@/.gitkeep
 
-$(ORG_PAGES):
+$(PAGES):
 	touch $@
 
-init: $(DIRECTORIES) $(ORG_PAGES)
+init: $(DIRECTORIES) $(PAGES)
 
 .PHONY: init
